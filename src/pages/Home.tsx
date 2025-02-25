eece1366c5881a93a0faa0b3a04ee1b0b361413e
KaueFonseca <kauedafonsecasantos2@gmail.com>
2025-02-25 08:06:32 -0300

    feat(develop): style text on pokedex header using styled components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import ButtonLoadMore from "../components/ButtonLoadMore";
+import styled from "styled-components";
 
 interface Pokemon {
   name: string;
@@ -44,26 +45,98 @@ const Home = () => {
 
   return (
     <div>
-      <h1>Pokémon's List</h1>
-      <ul>
-        {pokemons.map((poke, index) => {
-          const id = getPokemonId(poke.url);
-          return (
-            <li key={index}>
-              <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-                alt={poke.name}
-                width={80}
-                height={80}
-              />
-              <p>{poke.name}</p>
-            </li>
-          );
-        })}
-      </ul>
-      <ButtonLoadMore onClick={handleLoadMore} isLoading={loading} hasMore={hasMore} />
+      <Header>
+        <TitleH1>Pokédex</TitleH1>
+        <div>
+        <TitleH2>Find Your</TitleH2>
+        <Th2>Favorite Pokémon</Th2>
+          <button>O</button><input type="text" />
+        </div>
+
+      </Header>
+
+      <Section>
+        <UL>
+          {pokemons.map((poke, index) => {
+            const id = getPokemonId(poke.url);
+            return (
+              <LI key={index}>
+                <img
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                  alt={poke.name}
+                  width={90}
+                  height={90}
+                />
+                <p>{poke.name}</p>
+              </LI>
+            );
+          })}
+        </UL>
+        <ButtonLoadMore onClick={handleLoadMore} isLoading={loading} hasMore={hasMore} />
+      </Section>
     </div>
   );
 };
+
+const Header = styled.header`
+    color: #fff;
+    background-color: red;
+    text-align: center;
+    height: 300px;
+    border-radius: 0 0 50px 50px;
+    display: flex;
+    flex-direction: column;
+`;
+
+const TitleH1 = styled.h1`
+    font-size: 22px;
+    font-weight: 400;
+    color: #fff;
+    text-align: start;
+    padding: 10px;
+    padding-bottom: 90px;
+`;
+const TitleH2 = styled.h2`
+    font-size: 30px;
+    font-weight: 500;
+    color: #fff;
+    text-align: start;
+    padding: 0 60px;
+    line-height: 1;
+`;
+const Th2 = styled.h2`
+    font-size: 30px;
+    font-weight: 500;
+    color: #fff;
+    text-align: center;
+    padding: 0 20px;
+    
+`;
+
+const UL = styled.ul`
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+`;
+
+const LI = styled.li`
+  background-color: #f1f1f1;
+  list-style: none;
+  margin: 15px;
+
+  &:hover{
+    cursor: pointer;
+    background-color: rgb(210, 210, 210);
+  }
+`;
+
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    max-width: 1206px;
+    margin: 0 auto;
+`;
 
 export default Home;
