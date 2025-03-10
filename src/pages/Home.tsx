3d3a30e079dbdfe8a9a45a3abbc87d60ccec045e
KaueFonseca <kauedafonsecasantos2@gmail.com>
2025-03-10 09:04:13 -0300

    style(develop-header): style border radius of header, text, input and button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,11 +47,14 @@ const Home = () => {
     <div>
       <Header>
         <TitleH1>Pokédex</TitleH1>
-        <div>
-        <TitleH2>Find Your</TitleH2>
-        <Th2>Favorite Pokémon</Th2>
-          <button>O</button><input type="text" />
-        </div>
+        <DivHeader>
+          <TitleH2>Find Your</TitleH2>
+          <Th2>Favorite Pokémon</Th2>
+          <DivSearch>
+            <SearchButton>O</SearchButton>
+            <InputSearch type="text" placeholder="Pesquisar..." />
+          </DivSearch>
+        </DivHeader>
 
       </Header>
 
@@ -77,40 +80,80 @@ const Home = () => {
     </div>
   );
 };
-
+/* ---------- HEADER ----------*/
 const Header = styled.header`
-    color: #fff;
-    background-color: red;
+    background-color: #ff5656;
     text-align: center;
     height: 300px;
-    border-radius: 0 0 50px 50px;
     display: flex;
     flex-direction: column;
+    padding: 24px 16px;
 `;
 
 const TitleH1 = styled.h1`
-    font-size: 22px;
-    font-weight: 400;
-    color: #fff;
-    text-align: start;
-    padding: 10px;
-    padding-bottom: 90px;
+  font-size: 22px;
+  font-weight: 400;
+  color: #f9f9f9;
+  text-align: start;
+  padding: 0 0 90px 0;
 `;
 const TitleH2 = styled.h2`
     font-size: 30px;
     font-weight: 500;
-    color: #fff;
+    color: #f9f9f9;
     text-align: start;
-    padding: 0 60px;
+    padding: 0;
     line-height: 1;
 `;
 const Th2 = styled.h2`
     font-size: 30px;
     font-weight: 500;
-    color: #fff;
-    text-align: center;
-    padding: 0 20px;
+    color: #f9f9f9;
+    text-align: left;
+    padding: 0;
     
+`;
+
+const DivHeader = styled.div`
+  max-width: 350px;
+`;
+
+const DivSearch = styled.div`
+  max-width: 350px;
+  margin: 0 auto;
+  display: flex;
+  margin-top: 5px;
+`;
+
+
+const InputSearch = styled.input`
+  display: flex;
+  width: 100%;
+  margin: 0 auto;
+  height: 40px;
+  padding: 5px;
+  border: none;
+  border-radius: 0 20px 20px 0;
+`;
+
+const SearchButton = styled.button`
+  border: none;
+  padding: 10px;
+  border-radius: 20px 0 0 20px;
+  width: 50px;
+`;
+
+
+
+
+/* ---------- POKEMON LIST ----------*/
+
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    max-width: 1206px;
+    margin: 0 auto;
+    background-color: #ff5656;
 `;
 
 const UL = styled.ul`
@@ -119,24 +162,24 @@ const UL = styled.ul`
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
+    background-color: #fff;
+    padding: 20px 16px;
+    border-radius: 60px 60px 0 0;
 `;
 
 const LI = styled.li`
   background-color: #f1f1f1;
   list-style: none;
   margin: 15px;
+  width: 142px;
+  height: 142px;
+  text-align: center;
+  border-radius: 10px;
 
   &:hover{
     cursor: pointer;
     background-color: rgb(210, 210, 210);
   }
-`;
-
-const Section = styled.section`
-    display: flex;
-    flex-direction: column;
-    max-width: 1206px;
-    margin: 0 auto;
 `;
 
 export default Home;
