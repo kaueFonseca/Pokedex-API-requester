31dd5b686ead436f56da4be57323017c57b321b6
KaueFonseca <kauedafonsecasantos2@gmail.com>
2025-03-23 16:15:23 -0300

    style(develop): style background second page

--- src/components/PokemonDetails/PokemonDetailedPage.tsx
+++ src/components/PokemonDetails/PokemonDetailedPage.tsx
@@ -5,12 +5,21 @@ import { getPokemonID } from "../../services/pokemonService";
 import GoBackToHomeButton from "./GoBackToHomeButton/GoBackToHomeButton";
 import { getPokemonDescription } from "../../services/pokemonService";
 import styled from "styled-components";
-import PokeballBackground from "../../assets/pokeball_graphic_by_maratuna_d2joc5j.png";
+import { typeColors } from "../types/TypeColors";
+import PokeballBackground from "../../assets/pokeball-2-bright.png";
+import { lightenColor } from "../PokemonDetails/lightenPokemonColor/lightenColor";
+
 
 const PokemonDetailedPage = () => {
+  //Logic >>>
   const [pokemon, setPokemon] = useState<any>(null);
   const [description, setDescription] = useState("");
   const { id } = useParams();
+  //Style >>>
+  const primaryType = pokemon?.types[0]?.type?.name ?? "normal";
+  const bgColor = typeColors[primaryType] || "#FFFFFF";
+  const lighterColor = lightenColor(bgColor, 20);
+
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -30,29 +39,29 @@ const PokemonDetailedPage = () => {
 
   return (
     <>
-      <Header>
+      <Header bgColor={bgColor} lighterColor={lighterColor}>
         <Nav>
           <GoBackToHomeButton />
           {pokemon ? <H1>{pokemon.name} <H1span>{pokemon.id}</H1span></H1> : <p>Carregando...</p>}
 
         </Nav>
-        {pokemon ? (
-          <div>
-            {pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default && (
-              <img width={170} src={pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default} alt={pokemon.name} />
-            )}
-          </div>
-        ) : (
-          <p>Carregando...</p>
-        )}
-        <ul>
-          {pokemon?.types.map((type: any) => (
-            <LItypes key={type.type.name}>{type.type.name}</LItypes>
-          ))}
-        </ul>
+        <DivPokemon >
+          {pokemon ? (
+            <div>
+              {pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default && (
+                <img width={260} src={pokemon.sprites.versions["generation-v"]["black-white"].animated.front_default} alt={pokemon.name} />
+              )}
+            </div>
+          ) : (
+            <p>Carregando...</p>
+          )}
+          <ul>
+            {pokemon?.types.map((type: any) => (
+              <LItypes bgColor={bgColor} key={type.type.name}>{type.type.name}</LItypes>
+            ))}
+          </ul>
+        </DivPokemon>
       </Header>
-
-
       <main>
 
         <P>{description}</P>
@@ -88,16 +97,28 @@ const PokemonDetailedPage = () => {
   );
 };
 
-const Header = styled.header`
-  background: red; 
+
+const Header = styled.header<{ bgColor: string, lighterColor: string }>`
+  background: linear-gradient(to top, ${(props) => props.bgColor}, ${(props) => props.lighterColor}); 
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-image: url(${PokeballBackground});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: 100%;
   padding: 20px 0;
+
+  &:before{
+   content: "";
+    position: absolute;
+    width: 380px;
+    height: 380px;
+    top: 260px;
+    left: 196px;
+    background-image: url(${PokeballBackground});
+    background-size: contain;
+    background-repeat: no-repeat;
+    transform: translate(-50%, -50%);
+    opacity: 0.3; 
+    z-index: 0;
+  }
 `;
 
 const Nav = styled.nav`
@@ -128,9 +149,17 @@ const H1span = styled.span`
   content: 'ⵌ';
   }
 `
-
-const LItypes = styled.li`
-background: #000;
+const DivPokemon = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  z-index: 1;
+  width: 100%;    
+  padding: 130px 0 0 0;      
+  `;
+const LItypes = styled.li<{ bgColor: string }>`
+background: ${(props) => props.bgColor};
 color: #fff;
 border-radius: 22px;
 padding: 3px 10px;
